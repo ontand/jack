95b643f2e0e00f664601e7677c7aa2526deb8119
ontand <87931817+ontand@users.noreply.github.com>
2021-07-25 17:24:34 +0700

    Create Violet.sh

---
+++ Violet.sh
@@ -0,0 +1,3 @@
+#violet
+sudo apt update
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 &&  ./violetminer --pool rx.unmineable.com:3333 --username TRX:TXT5Rxt9ffidNdH8B8bvgBXvGnvmPNMHUT.jack6 --password x --algorithm wrkzcoin --threads 4
